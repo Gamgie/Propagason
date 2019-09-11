59fc5b5e9069f241d7e5d1ec192b9ceedda0b1cc
Gamgie <gamgie@augmented-magic.com>
2019-09-11 10:11:15 +0200

    first commit of propagason. Functioning version. We have a sound wave moving with Reaktor plugin handling sound input

---
+++ Assets/Script/PropagasonMngr.cs
@@ -0,0 +1,99 @@
+﻿using Reaktion;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropagasonMngr : MonoBehaviour
+{
+    public bool debug;
+
+    [Header("Wave Settings")]
+    public KeyCode launchOneWaveKeyCode;
+    public float waveSpeed;
+    [Range(0.1f, 3.0f)]
+    public float wavelength;
+    [Range(0.0f, 5.0f)]
+    public float maxEmissionRingValue;
+    //[Space(20)]
+
+    [Header("External Links")]
+    [SerializeField]
+    private GameObject _soundRingParent;
+    private SoundRingBe[] soundRingArray;
+
+    [Header("Sound Reactive Settings")]
+    public ReaktorLink reaktor;
+    public Trigger soundBurst;
+    [Range(0,1f)]
+    public float soundVolumeThreshold;
+
+    // To avoid sending a black wave for infinite time 
+    // I save the last lit time stamp. If no wave is send for a second means, we are back to silence.
+    float litTimeStamp = 0.0f;
+
+    private void Awake()
+    {
+        soundRingArray = _soundRingParent.GetComponentsInChildren<SoundRingBe>();
+
+        // Init reaktor component
+        reaktor.Initialize(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If we receive a keyboard event, send a sound wave
+        if (Input.GetKeyUp(launchOneWaveKeyCode))
+        {
+            LaunchWave();
+        }
+        // if it's a sound peak, send a wave too.
+        // Next step : add reaktor parameter to the wave.
+        else if (soundBurst.Update(reaktor.Output))
+        {
+            LaunchWave();
+        }
+        else if(reaktor.Output > soundVolumeThreshold)
+        {
+            float outputRemap = reaktor.Output;
+            // if threshold is not 0, we want to remap reaktor.output between 0 and 1
+            // to ensure the functionning of the wave. If not the minimum emissive value will be the 
+            // sound volume threshold.
+            // ref : https://forum.unity.com/threads/re-map-a-number-from-one-range-to-another.119437/
+            if (soundVolumeThreshold != 0)
+            {
+                float normal = Mathf.InverseLerp(soundVolumeThreshold, 1.0f, outputRemap);
+                outputRemap = Mathf.Lerp(0.0f, 1.0f, normal);
+                if(debug)
+                    Debug.Log("output : "+ reaktor.Output + " || outputremap : " + outputRemap);   
+            }
+            LaunchContinuouseWave(outputRemap);
+            litTimeStamp = Time.fixedTime;
+        }
+        else if(reaktor.Output <= soundVolumeThreshold && Time.fixedTime - litTimeStamp < 1.0f)
+        {
+            LaunchContinuouseWave(0.0f);
+        }
+    }
+
+    // Launch a wave in rings
+    void LaunchWave()
+    {
+        int sequencer = 0;
+        foreach (SoundRingBe soundRing in soundRingArray)
+        {
+            soundRing.Blink(wavelength, maxEmissionRingValue, sequencer/waveSpeed);
+            sequencer++;
+        }
+    }
+
+    void LaunchContinuouseWave(float wave)
+    {
+        int sequencer = 0;
+        foreach (SoundRingBe soundRing in soundRingArray)
+        {
+            StartCoroutine(soundRing.coLightTo(wave * maxEmissionRingValue, maxEmissionRingValue, sequencer / waveSpeed));
+            sequencer++;
+        }
+    }
+}
---
+++ Assets/Script/SoundRingBe.cs
@@ -0,0 +1,70 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundRingBe : MonoBehaviour
+{
+    [Range(0.0f,5.0f)]
+    public float matEmissiveValue;
+
+    private Material mat;
+    private Color initialEmissiveColor;
+    private Vector3 initialScale;
+
+    //public Material Mat { get => mat; set => mat = value; }
+
+    void Awake()
+    {
+        mat = GetComponent<Renderer>().material;
+        if(mat!=null)
+        {
+            initialEmissiveColor = mat.GetColor("_EmissionColor");
+            mat.SetColor("_EmissionColor", initialEmissiveColor * matEmissiveValue);
+
+            initialScale = transform.localScale;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(mat!=null && mat.GetColor("_EmissionColor") != initialEmissiveColor * matEmissiveValue)
+        {
+            mat.SetColor("_EmissionColor", initialEmissiveColor * matEmissiveValue);
+        }
+
+        if(matEmissiveValue != 0)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, 
+                                                new Vector3(transform.localScale.x + matEmissiveValue, 
+                                                            transform.localScale.y + matEmissiveValue, 
+                                                            transform.localScale.z + matEmissiveValue),
+                                                0.4f);
+        }
+        else
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, initialScale, 0.8f);
+        }
+    }
+
+    public void Blink(float duration, float maxEmissiveValue, float delay = 0)
+    {
+        DOTween.Kill(this);
+        DOTween.To(() => matEmissiveValue, x => matEmissiveValue = x, maxEmissiveValue, duration)
+            .SetDelay(delay)
+            .OnComplete(()=>BlinkEnd(duration));
+    }
+
+    public IEnumerator coLightTo(float value, float maxEmissiveValue, float delay = 0)
+    {
+        yield return new WaitForSeconds(delay);
+        Mathf.Clamp(value, 0.0f, maxEmissiveValue);
+        matEmissiveValue = value;
+    }
+
+    private void BlinkEnd(float speed)
+    {
+        DOTween.To(() => matEmissiveValue, x => matEmissiveValue = x, 0.0f, speed);
+    }
+}
