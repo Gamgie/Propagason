bfdf29e603ec35ebeacbf4052ec7a2bf9e5061eb
Gamgie <gamgie@augmented-magic.com>
2020-02-24 14:10:01 +0100

    add OSCJack to project. We now receive audio data from Chataigne. AudioMngr get those data via OSCMngr

---
+++ Assets/Script/AudioMngr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioMngr : MonoBehaviour
+{
+    public enum NoteType { C, CS, D, DS, E, F, FS, G, GS, A, AS, B };
+
+    //[Header("Sound Properties")]
+    public int Pitch { get => _pitch;
+        set
+        {
+            _pitch = value;
+            Note = (NoteType)(_pitch%12);
+        }
+    }
+    public float Frequency { get => _frequency; set => _frequency = value; }
+    public NoteType Note { get => _note; set => _note = value; }
+    public float Volume { get => _volume; set => _volume = value; }
+
+    [SerializeField] private int _pitch;
+    [SerializeField] private float _frequency;
+    [SerializeField] private NoteType _note;
+    [SerializeField] private float _volume;
+
+    public float volumeThreshold = 0.1f;
+
+}
--- Assets/Script/PropagasonMngr.cs
+++ Assets/Script/PropagasonMngr.cs
@@ -7,25 +7,17 @@ public class PropagasonMngr : MonoBehaviour
     public bool logInputLevel;
 
     [Header("Wave Settings")]
-    public KeyCode launchOneWaveKeyCode;
     public float waveSpeed;
-    [Range(0.1f, 3.0f)]
+    [Range(0.1f, 1.0f)]
     public float wavelength;
     [Range(0.0f, 5.0f)]
     public float maxEmissionRingValue;
-    //[Space(20)]
 
     [Header("External Links")]
     [SerializeField]  private GameObject _soundRingParent;
     [SerializeField] private MainCameraMngr _mainCamera;
     private SoundRingBe[] _soundRingArray;
 
-    [Header("[LASP] Sound Reactive Settings")]
-    [Tooltip("Decide if you use raw RMS or Audio Level Tracker (normalized) value.")]
-    public bool useNormalizedLevel;
-    [SerializeField] Lasp.FilterType _filterType;
-    [Tooltip("In case, you wanna filter sound input to avoid external noise for example. Use threshold.")]
-    [SerializeField, Range(0.0f, 1.0f)] float soundThreshold = 0.0f;
     
     public float normalizedLevel { get; set; }
 
@@ -38,71 +30,33 @@ public class PropagasonMngr : MonoBehaviour
     ParticleSystem.EmissionModule _finalEmbersEmission;
     [SerializeField] float _finalEmberEmiterMultiplier;
 
-
-
     private void Awake()
     {
         _soundRingArray = _soundRingParent.GetComponentsInChildren<SoundRingBe>();
         _finalEmbersEmission = _finalEmbersPS.emission;
         _finalEmbersEmission.rateOverTime = 0.0f;
-
-        GetComponent<Lasp.AudioLevelTracker>().filterType = _filterType;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // If we receive a keyboard event, send a sound wave
-        if (Input.GetKeyUp(launchOneWaveKeyCode))
-        {
-            LaunchWave();
-        }
-        else if(Input.GetKeyUp(_launchCameraPanning))
+        // If we receive a keyboard event
+        if(Input.GetKeyUp(_launchCameraPanning))
         {
             _timelineMngr.PlayCameraPanning();
         }
 
-        float inputLevel = 0.0f;
-        if(!useNormalizedLevel)
-        {
-            // We use raw RMS decibel value
-            inputLevel = Lasp.MasterInput.CalculateRMSDecibel(_filterType);
-            inputLevel = Mathf.Clamp01(1 - inputLevel / (-40)); // clamp it considering -40Db is silence level.
+        float inputLevel = normalizedLevel;
 
-            if (logInputLevel)
-                Debug.Log("RMS : " + logInputLevel);
-        }
-        else
-        {
-            inputLevel = normalizedLevel;
-
-            if (logInputLevel)
-                Debug.Log("Normalized level : " + normalizedLevel);
-        }
+        if (logInputLevel)
+            Debug.Log("Normalized level : " + normalizedLevel);
     
-        // Remap RMS value according to threshold
-        if(soundThreshold != 0)
-        {
-            float normal = Mathf.InverseLerp(soundThreshold, 1.0f, inputLevel);
-            inputLevel = Mathf.Lerp(0.0f, 1.0f, normal);
-        }
-
         LaunchContinuouseWave(inputLevel);
 
         // Send RMS value to camera for camera movement.
         _mainCamera.AnimateCamera(inputLevel);
     }
 
-    // Launch one wave in rings
-    void LaunchWave()
-    {
-        int sequencer = 0;
-        foreach (SoundRingBe soundRing in _soundRingArray)
-        {
-            soundRing.Blink(wavelength, maxEmissionRingValue, sequencer/waveSpeed);
-            sequencer++;
-        }
-    }
 
     void LaunchContinuouseWave(float wave)
     {
